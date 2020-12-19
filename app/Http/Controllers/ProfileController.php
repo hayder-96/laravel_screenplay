08380a7fe036567e457c37757618192dd16c1d6e
hayder ali <hxhwe1924@gmail.com>
2020-12-19 14:04:22 +0300

    Procfile for Heroku

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -30,7 +30,7 @@ class ProfileController extends BaseController
     public function indexOne()
     {
        
-        $users=profile::where('user_id',Auth::id());
+        $users=profile::all()->where('user_id',Auth::id());
 
         return $this->Respone(new users($users),'getOne');
     }
