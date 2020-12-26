4702f794cbae30f03fac8aa9ec2f242188b7533a
hayder ali <hxhwe1924@gmail.com>
2020-12-26 15:54:42 +0300

    Procfile for Heroku

--- routes/api.php
+++ routes/api.php
@@ -42,7 +42,7 @@ Route::middleware('auth:api')->group(function(){
     
     Route::resource('friend',FriendController::class);
     Route::post('getfriend',[FriendController::class,'input']);
-    Route::post('getitemfriend/{id}',[FriendController::class,'getItem']);
+    Route::get('getitemfriend/{id}',[FriendController::class,'getItem']);
 });
 
 
