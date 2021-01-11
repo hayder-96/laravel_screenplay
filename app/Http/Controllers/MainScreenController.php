a83ae6446d8a4608c8a966b238c5d66aabd489a2
hayder ali <hxhwe1924@gmail.com>
2021-01-12 00:40:24 +0300

    Procfile for Heroku

--- app/Http/Controllers/MainScreenController.php
+++ app/Http/Controllers/MainScreenController.php
@@ -144,8 +144,26 @@ class MainScreenController extends BaseController
         }
 
 
-        $uss->title=$input['title'];
+    //    $po=Cloudinary::find($uss->image);
+
+      
+    //    if($po!=$request->image){
+
+    //     Cloudinary::delete($po);
+    //     $poo=$request->image;
+    //     $path= Cloudinary::upload($poo->getRealPath())->getSecurePath();
+    //     $uss->image=$path;
+    //    }else{
+    //     $uss->image=$request->image;
+    //    }
+     $poo=$request->file('image');
+     $path= Cloudinary::upload($poo->getRealPath())->getSecurePath();
+
+        
        
+      //  $po=$path;
+        $uss->title=$input['title'];
+       // $uss->image=$path;
         $uss->user_id=Auth::id();
         $uss->save();
 
