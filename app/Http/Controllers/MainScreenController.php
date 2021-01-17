d894dc489c495ed453ae08f1e442b89a67a78f28
hayder ali <hxhwe1924@gmail.com>
2021-01-17 10:49:07 +0300

    Procfile for Heroku

--- app/Http/Controllers/MainScreenController.php
+++ app/Http/Controllers/MainScreenController.php
@@ -86,7 +86,7 @@ class MainScreenController extends BaseController
       //$path=(new UploadApi())->upload($newphoto);
 
       // $path=cloudinary()->upload($request->file($photo)->getRealPath())->getSecurePath();
-      $path= Cloudinary::upload($request->file('image')->getRealPath(),array("public_id" =>$request->title),array("width"=>60))->getSecurePath();
+      $path= Cloudinary::upload($request->file('image')->getRealPath(),array("public_id" =>$request->title), array("quality"=>"auto"))->getSecurePath();
       
     }
        
