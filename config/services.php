e7fa06c539adf82ce8a2935eff917db5f180afc1
hayder ali <hxhwe1924@gmail.com>
2021-01-24 19:59:43 +0300

    Procfile for Heroku

--- config/services.php
+++ config/services.php
@@ -16,7 +16,7 @@ return [
 
     'mailgun' => [
         'domain' =>'sandboxfa5dffa5c5904d259f1ad8e5dd9dbafb.mailgun.org',
-        'secret' =>'key-ba49790789ab407c3efe23ee01040edd',
+        'secret' =>'hxhwe1924@gmail.com',
         'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
     ],
 
