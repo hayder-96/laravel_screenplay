a578ce9d2a8d42d77676d4d8cfd99f802c661cfc
hayder ali <hxhwe1924@gmail.com>
2021-01-25 21:35:10 +0300

    Procfile for Heroku

--- config/mail.php
+++ config/mail.php
@@ -46,7 +46,7 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST','smtp.mailgun.org'),
+            'host' => env('MAIL_HOST','smtp.mailgun.gmail.org'),
             'port' => env('MAIL_PORT',587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
