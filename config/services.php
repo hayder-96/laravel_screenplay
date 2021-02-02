21069b6e39e9ed2f56af6011d5545cc2911f5a6f
hayder ali <hxhwe1924@gmail.com>
2021-02-02 18:19:48 +0300

    Procfile for Heroku

--- config/services.php
+++ config/services.php
@@ -14,6 +14,14 @@ return [
     |
     */
 
+    'sendgrid' => [
+        'api_key' => env('SENDGRID_API_KEY'),
+    ],
+
+
+
+
+
     'mailgun' => [
         'domain' =>env('MAILGUN_DOMAIN'),
         'secret' =>env('MAILGUN_SECRET'),
