881a8b1cadec5d9561f4c3ca2b0b56db54162ee5
hayder ali <hxhwe1924@gmail.com>
2021-02-11 12:54:37 +0300

    Procfile for Heroku

--- app/Http/Controllers/forget.php
+++ app/Http/Controllers/forget.php
@@ -25,7 +25,7 @@ class forget extends BaseController
     {
 
        
-        return Socialite::driver('facebook');
+        return Socialite::driver('facebook')->redirect();
     }
    
     public function handleGoogleCallback()
