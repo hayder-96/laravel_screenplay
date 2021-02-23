5d728658ee371abffad1861abc50358546046f74
hayder ali <hxhwe1924@gmail.com>
2021-02-23 21:53:57 +0300

    Procfile for Heroku

--- app/Http/Controllers/forget.php
+++ app/Http/Controllers/forget.php
@@ -26,11 +26,11 @@ class forget extends BaseController
    
     public function store(Request $request){
 
-    //   $user=User::where('email',$request->email)->first();
-    //   if($user->count()==0){
-    //     return $this->Respone(500,'no');
-    //     return;
-    //   }
+      $user=User::where('email',$request->email)->first();
+      if($user->email!=$request->email){
+        return $this->Respone(500,'no');
+        return;
+      }
 
     //     $input=$request->all();
       
